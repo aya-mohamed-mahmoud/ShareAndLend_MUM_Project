6e200c0e33cc32cdb587d6ef47f9b6eb6826fae1
aya <aya.zaki17390@gmail.com>
2019-12-15 23:10:47 -0600

    initial UI

--- app/src/main/java/com/example/shareandlend/MainActivity.kt
+++ app/src/main/java/com/example/shareandlend/MainActivity.kt
@@ -1,12 +1,81 @@
 package com.example.shareandlend
 
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.viewpager.widget.ViewPager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+
+       // actionBar?.setBackgroundDrawable(ColorDrawable(Color.parseColor("#0000ff")));
+
+        val adapter = ViewPagerAdapter(supportFragmentManager)
+        adapter.addFragment(HomeFragment(), "Home")
+        adapter.addFragment(ShareFragment(), "Share")
+        adapter.addFragment(LendFragment(), "Lend")
+
+        viewPager.adapter = adapter
+
+
+        viewPager.addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
+            // This method will be invoked when a new page becomes selected.
+            override fun onPageSelected(position: Int) {
+                Toast.makeText(
+                    this@MainActivity,
+                    "Selected page position: $position", Toast.LENGTH_SHORT
+                ).show()
+            }
+
+            // This method will be invoked when the current page is scrolled
+            override fun onPageScrolled(
+                position: Int,
+                positionOffset: Float,
+                positionOffsetPixels: Int
+            ) { // Code goes here
+            }
+
+            // Called when the scroll state changes:
+            // SCROLL_STATE_IDLE, SCROLL_STATE_DRAGGING, SCROLL_STATE_SETTLING
+            override fun onPageScrollStateChanged(state: Int) { // Code goes here
+            }
+        })
+        tabs.setupWithViewPager(viewPager)
     }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action1 -> {
+                Toast.makeText(
+                    this@MainActivity,
+                    "home ", Toast.LENGTH_SHORT
+                ).show()
+                true
+            }
+            R.id.action2 -> true
+            R.id.action3 -> true
+
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
 }
