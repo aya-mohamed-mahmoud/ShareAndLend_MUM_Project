70810b14995d0131400c0be8dcce01f1a189283a
Afify <a.heshamshaaban@gmail.com>
2019-12-15 23:49:26 -0600

    adding database logic with SugarORM

--- app/src/main/java/com/example/shareandlend/LendFragment.kt
+++ app/src/main/java/com/example/shareandlend/LendFragment.kt
@@ -35,10 +35,7 @@ class LendFragment : Fragment() {
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_lend, container, false)
     }
