6234db0cdf27e72cf831e0cb83b29d0299c4d1e3
Afify <a.heshamshaaban@gmail.com>
2019-12-19 04:41:54 -0600

    final commit

--- app/src/main/java/com/example/shareandlend/AvailableLendItemsFragment.kt
+++ app/src/main/java/com/example/shareandlend/AvailableLendItemsFragment.kt
@@ -1,6 +1,7 @@
 package com.example.shareandlend
 
 import android.os.Bundle
+import android.text.format.DateUtils
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,6 +11,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.shareandlend.model.Item
 import com.example.shareandlend.model.ShareType
 import com.google.firebase.database.*
+import java.text.SimpleDateFormat
 import java.util.*
 import kotlin.collections.ArrayList
 
@@ -39,9 +41,19 @@ class AvailableLendItemsFragment : Fragment() {
                 // Get Post object and use the values to update the UI
                 for (snapshot in dataSnapshot.children) {
                     item = snapshot.getValue(Item::class.java)
-                    if (item!!.type != null && item!!.type!! == ShareType.LEND.value && item!!.availableToDate!!.after(Date())) {
-                        items.add(item!!)
+                    val dateFormat =
+                        SimpleDateFormat("dd/MM/yyy")
+                    val fromDateStr  = dateFormat.format(item!!.availableFromDate)
+                    val fromFormatedDate = dateFormat.parse(fromDateStr)
+                    val toDateStr  = dateFormat.format(item!!.availableToDate)
+                    val toFormatedDate = dateFormat.parse(toDateStr)
+                    val nowDateStr = dateFormat.format(Date())
+                    val nowDate = dateFormat.parse(nowDateStr)
 
+                    if (item!!.type != null && item!!.type!! == ShareType.LEND.value ){
+                        if(fromFormatedDate.compareTo(nowDate) >= 0 && toFormatedDate.compareTo(nowDate) >= 0) {
+                            items.add(item!!)
+                        }
                     }
                 }
                 val rview: RecyclerView = view.findViewById(R.id.rv) as RecyclerView
--- app/src/main/java/com/example/shareandlend/AvailableSharedItemsFragment.kt
+++ app/src/main/java/com/example/shareandlend/AvailableSharedItemsFragment.kt
@@ -1,17 +1,18 @@
 package com.example.shareandlend
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.shareandlend.model.Item
 import com.example.shareandlend.model.ShareType
 import com.google.firebase.database.*
+import java.text.DateFormat
+import java.text.SimpleDateFormat
 import java.util.*
-import kotlin.collections.ArrayList
 
 class AvailableSharedItemsFragment : Fragment() {
 
@@ -28,7 +29,6 @@ class AvailableSharedItemsFragment : Fragment() {
         val view = inflater.inflate(R.layout.available_shared_items_fragment, container, false)
 
         val context = inflater.context
-        val rview: RecyclerView = view.findViewById(R.id.rv) as RecyclerView
 
         database.child("Items")
             .addValueEventListener(object : ValueEventListener {
@@ -38,9 +38,19 @@ class AvailableSharedItemsFragment : Fragment() {
                     // Get Post object and use the values to update the UI
                     for (snapshot in dataSnapshot.children) {
                         item = snapshot.getValue(Item::class.java)
-                        if (item!!.type != null && item!!.type!! == ShareType.SHARE.value && item!!.availableToDate!!.after(Date())) {
-                                items.add(item!!)
+                        val dateFormat =
+                            SimpleDateFormat("dd/MM/yyy")
+                        val fromDateStr  = dateFormat.format(item!!.availableFromDate)
+                        val fromFormatedDate = dateFormat.parse(fromDateStr)
+                        val toDateStr  = dateFormat.format(item!!.availableToDate)
+                        val toFormatedDate = dateFormat.parse(toDateStr)
+                        val nowDateStr = dateFormat.format(Date())
+                        val nowDate = dateFormat.parse(nowDateStr)
 
+                        if (item!!.type != null && item!!.type!! == ShareType.SHARE.value ){
+                            if(fromFormatedDate.compareTo(nowDate) >= 0 && toFormatedDate.compareTo(nowDate) >= 0) {
+                                items.add(item!!)
+                            }
                         }
                     }
 
